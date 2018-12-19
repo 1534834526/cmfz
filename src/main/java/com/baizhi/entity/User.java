5bb2858f5b9e88471a2ae58f8e5c7c3f6f9e7f82
hangaoming <1534834526@qq.com>
2018-12-19 19:07:59 +0800

    cmfz 环境搭建成功 V1.0

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,31 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements Serializable {
+    private Integer id;
+    private String phone;
+    private String password;
+    private String salt;
+    private String sign;
+    private String head_pic;
+    private String name;
+    private String dharma;
+    private String sex;
+    private String province;
+    private String city;
+    private Integer status;
+    @Column(name = "reg_date")
+    private Date regDate;
+}
